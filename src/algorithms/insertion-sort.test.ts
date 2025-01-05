c3d0bc6f5a0f85192f1e4e52f010070687319ce3
Lucas Doell <lucas@lux.dev>
2025-01-05 02:58:57 -0500

    Add reverse insertion sort

--- src/algorithms/insertion-sort.test.ts
+++ src/algorithms/insertion-sort.test.ts
@@ -1,9 +1,16 @@
 import { expect, test } from "vitest";
-import { insertionSort } from "./insertion-sort";
+import { insertionSort, reverseInsertionSort } from "./insertion-sort";
 
 test("array is sorted", () => {
   const array = [5, 2, 4, 6, 1, 3];
   insertionSort(array);
 
   expect(array).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test("array is sorted in decreasing order", () => {
+  const array = [5, 2, 4, 6, 1, 3];
+  reverseInsertionSort(array);
+
+  expect(array).toEqual([6, 5, 4, 3, 2, 1]);
 });
--- src/algorithms/insertion-sort.ts
+++ src/algorithms/insertion-sort.ts
@@ -19,3 +19,26 @@ export function insertionSort(array: number[]) {
     array[j + 1] = key;
   }
 }
+
+/**
+ * Sorts the given array in-place using the reverse insertion sort algorithm.
+ *
+ * Reverse insertion sort is a variation of the insertion sort algorithm that
+ * sorts the array in descending order. It builds the final sorted array one
+ * item at a time, inserting each new element into its correct position in the
+ * sorted portion of the array.
+ *
+ * @param array - The array to be sorted in descending order.
+ */
+export function reverseInsertionSort(array: number[]) {
+  for (let i = 1; i < array.length; i++) {
+    const key = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] < key) {
+      array[j + 1] = array[j];
+      j -= 1;
+    }
+    array[j + 1] = key;
+  }
+}
